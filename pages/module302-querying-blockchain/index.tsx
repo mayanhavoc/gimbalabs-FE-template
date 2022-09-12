63e483e608ecab808e7e4af74a1b10bb83e73da3
jamesdunseith <jdunseith@gimbalabs.io>
2022-09-12 16:18:52 -0400

    Update Module 302

--- pages/module302-querying-blockchain/index.tsx
+++ pages/module302-querying-blockchain/index.tsx
@@ -9,29 +9,63 @@ import MetadataExampleQuery from '../../components/queryResults/metadataExampleQ
 const Module302: NextPage = () => {
     return (
         <Box>
-            <Heading>Module 302: Querying the Blockchain</Heading>
-            <Grid templateColumns='repeat(2, 1fr)' gap='3' pt='5'>
-                <GridItem bg='purple.100' _hover={{ bg: 'purple.800', color: 'white' }}>
-                    <Text p='3'>
-                        <Link href="/module302-querying-blockchain/302-mastery-level-1">Mastery Level 1</Link>
-                    </Text>
-                </GridItem>
-                <GridItem bg='purple.100' _hover={{ bg: 'purple.800', color: 'white' }}>
-                    <Text p='3'>
-                        <Link href="/module302-querying-blockchain/302-mastery-level-2">Mastery Level 2</Link>
-                    </Text>
-                </GridItem>
-                <GridItem bg='purple.100' _hover={{ bg: 'purple.800', color: 'white' }}>
-                    <Text p='3'>
-                        <Link href="/module302-querying-blockchain/302-mastery-level-3">Mastery Level 3</Link>
-                    </Text>
-                </GridItem>
-                <GridItem bg='purple.100' _hover={{ bg: 'purple.800', color: 'white' }}>
-                    <Text p='3'>
-                        <Link href="/module302-querying-blockchain/302-mastery-level-4">Mastery Level 4</Link>
-                    </Text>
-                </GridItem>
-            </Grid>
+            <Heading py='3'>Module 302: Querying the Blockchain</Heading>
+            <Text py='3' fontSize='xl'>
+                This Module is all about writing queries to get information from the Cardano blockchain. Because data is only useful if we DO something with it, it will also be necessary to investigate Javascript and front-end design to help us use the data.
+            </Text>
+            <Text p='3' fontSize='xl'>
+                <Link href="/module302-querying-blockchain/302-mastery-level-1">
+                    <ChakraLink>
+                        Mastery Level 1: Customize and Style a Query
+                    </ChakraLink>
+                </Link>
+                &nbsp;
+                <Link href="https://gimbalabs.instructure.com/courses/26/assignments/465">
+                    <ChakraLink color='purple.700'>
+                        (View on Canvas)
+                    </ChakraLink>
+                </Link>
+            </Text>
+            <Text p='3' fontSize='xl'>
+                <Link href="/module302-querying-blockchain/302-mastery-level-2">
+                    <ChakraLink>
+                        Mastery Level 2: Rewrite a Koios Query with GraphQL
+                    </ChakraLink>
+                </Link>
+                &nbsp;
+                <Link href="https://gimbalabs.instructure.com/courses/26/assignments/466">
+                    <ChakraLink color='purple.700'>
+                        (View on Canvas)
+                    </ChakraLink>
+                </Link>
+            </Text>
+            <Text p='3' fontSize='xl'>
+                <Link href="/module302-querying-blockchain/302-mastery-level-3">
+                    <ChakraLink>
+                        Mastery Level 3: Create a Passive Component
+                    </ChakraLink>
+                </Link>
+                &nbsp;
+                <Link href="https://gimbalabs.instructure.com/courses/26/assignments/467">
+                    <ChakraLink color='purple.700'>
+                        (View on Canvas)
+                    </ChakraLink>
+                </Link>
+            </Text>
+            <Text p='3' fontSize='xl'>
+                <Link href="/module302-querying-blockchain/302-mastery-level-4">
+                    <ChakraLink>
+                        Mastery Level 4: Create a Transaction Component
+                    </ChakraLink>
+                </Link>
+                &nbsp;
+                <Link href="https://gimbalabs.instructure.com/courses/26/assignments/468">
+                    <ChakraLink color='purple.700'>
+                        (View on Canvas)
+                    </ChakraLink>
+                </Link>
+            </Text>
+
             <Heading py='5'>Some (Not-So-Pretty) Examples</Heading>
             <MetadataExampleQuery />
             <HoldersByAssetID />
